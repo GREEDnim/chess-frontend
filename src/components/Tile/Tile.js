e9cc62eee7bc61d6ebc3e9c21725f0eefd7912e9
GREEDnim <sivadhas007@gmail.com>
2023-06-14 00:31:44 +0530

    made initial board

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -3,6 +3,7 @@ function Tile({x,y,imageSrc}){
 
     const className=`tile ${ (x+y)%2===0? 'white' : 'black'}`;
     return <div className={className}>
+        {imageSrc?<img src={imageSrc}></img>:null}
     </div>
 }
 export default Tile;
