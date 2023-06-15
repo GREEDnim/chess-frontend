b3da2b05b716f3b0c6c0fc13718ebac436e54c45
GREEDnim <sivadhas007@gmail.com>
2023-06-15 09:49:22 +0530

    initial board made

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,87 +1,15 @@
 import {useState} from 'react'
-import Tile from '../Tile/Tile';
-import bPawn from '../../assets/bPawn.svg';
-import bKing from '../../assets/bKing.svg';
-import bQueen from '../../assets/bQueen.svg';
-import bRook from '../../assets/bRook.svg';
-import bKnight from '../../assets/bKnight.svg';
-import bBishop from '../../assets/bBishop.svg';
-import wPawn from '../../assets/wPawn.svg';
-import wKing from '../../assets/wKing.svg';
-import wQueen from '../../assets/wQueen.svg';
-import wRook from '../../assets/wRook.svg';
-import wKnight from '../../assets/wKnight.svg';
-import wBishop from '../../assets/wBishop.svg';
+import { getBoard,createInitialBoard } from '../../Services/BoardService';
 import './Board.css';
+import Tile from '../Tile/Tile';
 function Board(){
 
-    function getBoard(){
-        let board=[];
-        for(let x=0;x<8;x++){
-            let row=[];
-            for(let y=0;y<=7;y++){
-                row.push({ x,y,imageSrc:""})
-            }
-            board.push(row);
-        }
-        return board;
-    }
-    function createInitialBoard(board){
-
-        //white and black rows
-        let white=6;
-        let black=1;
-        
-        for(let j=0;j<8;j++){
-            board[white][j].imageSrc=wPawn;
-            board[black][j].imageSrc=bPawn;
-        }
-
-        let s=0;
-        let e=7;
-        white=7;
-        black=0;
-        while(s<e){
-            if(s==0) {
-                board[white][s].imageSrc=wRook;
-                board[white][e].imageSrc=wRook;
-                board[black][s].imageSrc=bRook;
-                board[black][e].imageSrc=bRook;
-            }
-            if(s==1){
-                board[white][s].imageSrc=wKnight;
-                board[white][e].imageSrc=wKnight;
-                board[black][s].imageSrc=bKnight;
-                board[black][e].imageSrc=bKnight;
-            }
-            if(s==2){
-                board[white][s].imageSrc=wBishop;
-                board[white][e].imageSrc=wBishop;
-                board[black][s].imageSrc=bBishop;
-                board[black][e].imageSrc=bBishop;        
-            }
-            if(s==3){
-                board[white][s].imageSrc=wQueen;
-                board[white][e].imageSrc=wKing;
-                board[black][s].imageSrc=bQueen;
-                board[black][e].imageSrc=bKing; 
-            }
-
-            s++;
-            e--;
-        }
-        return board;
-
-
-
-
-    }
     const[board,setBoard]=useState(createInitialBoard(getBoard()));
 
     return(
         <div className='board-container'>
-            {board.map((row)=>{
-                return row.map((ele)=><Tile x={ele.x} y={ele.y} imageSrc={ele.imageSrc}/>)
+            {board.map((row,x)=>{
+                return row.map((ele,y)=><Tile x={x} y={y} imageSrc={ele.imageSrc}/>)
             })}
         </div>
     )
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,9 +1,10 @@
 import './Tile.css'
+import Piece from '../Piece/Piece';
 function Tile({x,y,imageSrc}){
 
     const className=`tile ${ (x+y)%2===0? 'white' : 'black'}`;
-    return <div className={className}>
-        {imageSrc?<div className="chess-piece" style={ {backgroundImage:`url(${imageSrc})`} } ></div>:null}
+    return <div className={className} data-x={x} data-y={y} key={`${x}-${y}`} >
+        {imageSrc?<Piece imageSrc={imageSrc}></Piece>:null}
     </div>
 }  
 export default Tile;
