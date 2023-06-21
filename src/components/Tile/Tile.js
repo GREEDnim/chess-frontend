73ec7537dea71f3b9aa313ea85f1527ef805c61e
GREEDnim <sivadhas007@gmail.com>
2023-06-22 01:04:48 +0530

    data transfer between multiple nodes

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,11 +1,16 @@
 import './Tile.css';
 import Piece from '../Piece/Piece';
-import { validMove } from '../../Services/Pieces/PieceService';
-import { checkMate } from '../../Services/Board/CheckMate';
-import { ChessPiece } from '../../Model/ChessPiece';
-import { Game } from '../../Services/Board/Game';
 
-function Tile({ x, y, board, setBoard , gameOver, setGameOver}) {
+
+import io from 'socket.io-client';
+const socket =io.connect('http://localhost:3001');
+
+
+function sendMoveToOpponent(from,to){
+socket.emit("send-piece",{from,to})
+}
+
+function Tile({ x, y,piece, validateAndAddToBoard, gameOver}) {
   
   function dragOver(e) {
     e.preventDefault();
@@ -15,31 +20,11 @@ function Tile({ x, y, board, setBoard , gameOver, setGameOver}) {
     e.preventDefault();
     const from= JSON.parse(e.dataTransfer.getData('text/plain'));
     const to={x,y}
-   
-    if( board[from.x][from.y].color != Game.getTurn()) return;
-    // console.log(from,to);
-    if(validMove(from,to,board)){
-      // console.log(validMove(from,to,board));
-      addToBoard(from,to);
-      // console.log(board)
-      const opponentColor=board[to.x][to.y].color==0?1:0;
-      // for every valid move made, check if the opp is checkmated.
-      if(checkMate( opponentColor,board )) setGameOver(true);
-
-      //changing the turn;
-      Game.changeTurn();
-    }
-    // console.log(from,to);
+    let valid=validateAndAddToBoard(from,to,true);
+    //im only sending the from and to ;
+    if(valid) sendMoveToOpponent(from,to);
   }
-
-  function addToBoard(from,to){
-    board[to.x][to.y]=board[from.x][from.y];
-    board[from.x][from.y] = new ChessPiece('I',-1,null,false);
-    setBoard([...board]);
-  }
-
   const className = `tile ${ (x+y)%2 === 0 ? 'white' : 'black'}`;
-  const piece=board[x][y];
   return (
     <div className={className} data-x={x} data-y={y} key={`${x}-${y}`} onDragOver={dragOver} onDrop={drop}>
       {piece.valid && 
