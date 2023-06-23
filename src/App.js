36c0dc327405b1b443f14f90e32f64a785f320b2
Sivadhas <84082104+GREEDnim@users.noreply.github.com>
2023-06-23 12:35:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css'
 import { useState } from "react";
 import { Route,Routes } from "react-router-dom";
 import io from 'socket.io-client';
-const socket =io.connect('http://localhost:3001');
+const socket =io.connect('https://chess-backend-znna.onrender.com');
 
 function App(){
   const [color, setColor] = useState(undefined);
